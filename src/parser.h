ab5985a3bd9ae1d8e74adb620675adca9686f111
Kelvin Liu <kelvinyliu@gmail.com>
2025-05-07 20:49:37 +0100

    Created token parser header file

---
+++ src/parser.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "ast.h"
+#include "tokeniser.h"
+
+struct ASTNode* createBinaryNode(char op, struct ASTNode* left, struct ASTNode* right);
+struct ASTNode* parsePrimary(struct TokenList* tokens, size_t* index);
+struct ASTNode* parseFactor(struct TokenList* tokens, size_t* index);
+struct ASTNode* parseTerm(struct TokenList* tokens, size_t* index);
+struct ASTNode* parseExpression(struct TokenList* tokens, size_t* index);
+struct ASTNode* parseStatement(struct TokenList* tokens, size_t* index);
+struct ASTNode* parseDeclaration(struct TokenList* tokens, size_t* index);
+struct ASTNode* parseAssignment(struct TokenList* tokens, size_t* index);
+struct ASTNodeList parseProgram(const char* sourceCode);
+
