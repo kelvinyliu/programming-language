f4f38fb9ecef88de6bf13417ea0ad3776cbde338
Kelvin Liu <kelvinyliu@gmail.com>
2025-05-07 20:50:15 +0100

    Created evaluator header file to read the AST and perform actions

---
+++ src/evaluator.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "ast.h"
+
+enum ValueType {
+    VALUE_NUMBER,
+    VALUE_TEXT,
+};
+
+struct Value {
+    enum ValueType type;
+    union {
+        double  number;
+        char*   text;
+    } data;
+};
+
+// Hashmap for environment, to store values
+
+struct Entry {
+    char            *key;
+    struct Value    value;
+    
+    // collision resolution, linked list strat
+    struct Entry*   next;
+};
+
+struct Environment {
+    struct Entry*   *bucket;
+    size_t          bucket_count;
+};
+
+// Environment
+void createEnvironment(struct Environment* env);
+void freeEnvironment(struct Environment* env);
+
+struct Value getValue(struct Environment* env, const char* key);
+void setValue(struct Environment* env, const char* key, struct Value val);
+
+// Evaluation
+struct Value createNumberValue(double num);
+struct Value createTextValue(char* str);
+struct Value evaluateASTNode(const struct ASTNode* node, struct Environment* env);
+void evaluateAST(const struct ASTNodeList* astList, struct Environment* env);
